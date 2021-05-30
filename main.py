b03834e59409b02d024380fd51ffc203ff10eb5e
Lilseec <44337171+Lilseec@users.noreply.github.com>
2021-05-30 19:27:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
-import os.path
+import os
 import time
+import requests
+import ffmpeg
 
 counter = 0
 x = input("Please enter song title: \n")
 y = input("Please enter artist name: \n")
+dirPath = x + y
+os.mkdir(dirPath)
+baseLink = 'https://www.youtube.com/results?search_query='
 lst = x.split() + y.split()
-endpoint = ''
+endpoint = 'https://www.youtube.com/results?search_query='
 for word in lst:
     endpoint = endpoint + word
-    endpoint = endpoint + ' '
+    endpoint = endpoint + '+'
     print(endpoint)
 
 options = webdriver.ChromeOptions()
@@ -24,29 +29,34 @@ options.add_experimental_option('prefs', {
 "plugins.always_open_pdf_externally": True
 })
 driver = webdriver.Chrome(executable_path='./chromedriver.exe', options=options)
-driver.get("https://ru-music.com/search/" + endpoint)
-delay = 5
+driver.get(endpoint)
+delay = 10
 
 try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "playlist-btn-down")))
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "ytd-video-renderer")))
     print ("Page is ready!")
 except TimeoutException:
-    print ("Loading took too much time!")
-elem = driver.find_element_by_class_name('playlist-btn-down')
+    corrected = driver.find_element_by_class_name('yt-search-query-correction')
+    corrected.click()
+    print("Loading took too much time!")
+elem = driver.find_element_by_class_name('ytd-thumbnail')
 link = elem.get_attribute('href')
-elem.click()
-window_after = driver.window_handles[1]
-driver.switch_to.window(window_after)
-btn = driver.find_element_by_id('dbutton')
-btn.click()
-
-closure = y + '-' + x + '.mp3'
-while not os.path.exists(closure):
-    time.sleep(1)
-    counter = counter + 1
-    if counter > 20:
-        driver.quit()
-        break
-if os.path.isfile(closure):
-    driver.quit()
+begining = link[:12]
+ending = link[link.find("?") + 1 : ]
+forSavefromnet = begining + 'ss' + 'youtube.com/watch?' + ending
+driver.get(forSavefromnet)
+try:
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "no-downloadable")))
+    print ("Page is ready!")
+except TimeoutException:
+    print("Loading took too much time!")
+downloadLink = driver.find_element_by_class_name('no-downloadable').get_attribute('href')
+print(downloadLink)
+driver.quit()
+video = requests.get(downloadLink)
+f = open(dirPath + '/song.mp4', 'wb')
+f.write(video.content)
+stream = ffmpeg.input(dirPath + '/song.mp4')
+stream = ffmpeg.output(stream, 'output.mp3')
+ffmpeg.run(stream)
 
