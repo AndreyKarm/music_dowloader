73b50d9a99421255c9e30756e276eb188dfcec32
Lilseec <44337171+Lilseec@users.noreply.github.com>
2021-06-03 00:28:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,58 +4,62 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 import os
-import shutil
 import requests
 import ffmpeg
+from zipfile import ZipFile
 
-counter = 0
-x = input("Please enter song title: \n")
-y = input("Please enter artist name: \n")
-dirPath = x + y
-if not y:
-    y = ' '
-os.mkdir(dirPath)
-baseLink = 'https://www.youtube.com/results?search_query='
-lst = x.split() + y.split()
-endpoint = 'https://www.youtube.com/results?search_query='
-for word in lst:
-    endpoint = endpoint + word
-    endpoint = endpoint + '+'
-    print(endpoint)
-endpoint = endpoint + 'Lyric'
 
-driver = webdriver.Chrome(executable_path='./chromedriver.exe')
-driver.get(endpoint)
-delay = 10
+x = input("Please enter song title and artist name(if multiple use ','): \n")
+listOfSongs = x.split(',')
+zipObj = ZipFile('songs.zip', 'w')
+for song in listOfSongs:
+    dirPath = song
 
-try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "ytd-video-renderer")))
-    print ("Page is ready!")
-except TimeoutException:
-    corrected = driver.find_element_by_class_name('yt-search-query-correction')
-    corrected.click()
-    print("Loading took too much time!")
-elem = driver.find_element_by_class_name('ytd-thumbnail')
-link = elem.get_attribute('href')
-begining = link[:12]
-ending = link[link.find("?") + 1 : ]
-forSavefromnet = begining + 'ss' + 'youtube.com/watch?' + ending
-driver.get(forSavefromnet)
-try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "no-downloadable")))
-    print ("Page is ready!")
-except TimeoutException:
-    print("Loading took too much time!")
-downloadLink = driver.find_element_by_class_name('no-downloadable').get_attribute('href')
-print(downloadLink)
-driver.quit()
-video = requests.get(downloadLink)
-f = open(dirPath + '/song.mp4', 'wb')
-f.write(video.content)
-f.close()
-stream = ffmpeg.input(dirPath + '/song.mp4')
-stream = ffmpeg.output(stream, y + '-' + x +'.mp3')
-ffmpeg.run(stream)
-os.remove(dirPath + '/song.mp4')
-os.rmdir(dirPath)
+    os.mkdir(dirPath)
+    baseLink = 'https://www.youtube.com/results?search_query='
+    lst = song.split()
+    endpoint = 'https://www.youtube.com/results?search_query='
+    for word in lst:
+        endpoint = endpoint + word
+        endpoint = endpoint + '+'
+        print(endpoint)
+    endpoint = endpoint + 'Lyric'
+
+    driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+    driver.get(endpoint)
+
+    try:
+        myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "ytd-video-renderer")))
+        print ("Page is ready!")
+    except TimeoutException:
+        corrected = driver.find_element_by_class_name('yt-search-query-correction')
+        corrected.click()
+        print("Loading took too much time!")
+    elem = driver.find_element_by_class_name('ytd-thumbnail')
+    link = elem.get_attribute('href')
+    begining = link[:12]
+    ending = link[link.find("?") + 1 : ]
+    forSavefromnet = begining + 'ss' + 'youtube.com/watch?' + ending
+    driver.get(forSavefromnet)
+    try:
+        myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "no-downloadable")))
+        print ("Page is ready!")
+    except TimeoutException:
+        print("Loading took too much time!")
+    downloadLink = driver.find_element_by_class_name('no-downloadable').get_attribute('href')
+    print('Dowloading ' + song)
+    driver.quit()
+    video = requests.get(downloadLink)
+    f = open(dirPath + '/song.mp4', 'wb')
+    f.write(video.content)
+    f.close()
+    stream = ffmpeg.input(dirPath + '/song.mp4')
+    stream = ffmpeg.output(stream, song +'.mp3')
+    ffmpeg.run(stream)
+    os.remove(dirPath + '/song.mp4')
+    os.rmdir(dirPath)
+    zipObj.write(song +'.mp3')
+    os.remove(song +'.mp3')
+
+zipObj.close()
 
